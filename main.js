6f9e0065735375c9e1cdf3b94c9d2c00e792ef24
sk8terdinz <d.refranto@gmail.com>
2023-05-12 16:57:16 +0700

    Adding draw polyline

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function init(){
     // Basemaps
     const openStreetMapStandard = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
+        noWrap: true,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })
 
@@ -21,12 +22,11 @@ function init(){
         minZoom: 4,
         zoomSnap: 0.25,
         zoomDelta: 0.25,
+        easeLinearity: 0.5,
+        worldCopyJump: true,
         layers: [stadiaMaps]
     })
 
-    mymap.on('zoom', function(e){
-        console.log(e.target._zoom)
-    })
 
     // Basemap Object
     const baseLayers = {
@@ -37,7 +37,7 @@ function init(){
     // Overlays
     const javaBaseMapImage = './Data/Java.png';
     const javaBaseMapBounds = [[-8.455748978281614, 104.065442496],[-5.674229705192159, 106.901065401]]
-    const imageJavaOverlay = L.imageOverlay(javaBaseMapImage, javaBaseMapBounds).addTo(mymap)
+    const imageJavaOverlay = L.imageOverlay(javaBaseMapImage, javaBaseMapBounds)
 
     // Overlay Object
     const overlayerLayers = {
@@ -50,10 +50,6 @@ function init(){
         position: 'topright'
     }).addTo(mymap)
 
-    mymap.on('click', function(e){
-        console.log(e.latlng)
-    })
-
     // Java marker
     const javaMarker = L.marker([-6.181483033062791, 106.82447481285564], {
         opacity: 1
@@ -61,4 +57,88 @@ function init(){
 
     const javaMarkerPoppup = javaMarker.bindPopup('Jakarta city from popup')
     const javaMarkerTooltip = javaMarker.bindTooltip('Jakarta city from tooltip').openTooltip()
+
+    // Geolocation API
+    /*mymap.locate({setView:true, maxZoom: 18})
+
+
+    function onLocationFound(e){
+        var radius = e.accuracy.toFixed(2)
+
+        var locationMarker = L.marker(e.latlng).addTo(mymap).bindPopup('You are within ' + radius + ' metres from this point').openPopup()
+
+        var locationCircle = L.circle(e.latlng, radius).addTo(mymap)
+    }
+
+    mymap.on('locationfound', onLocationFound)
+
+
+    function onLocationError(e){
+        window.alert(e.message)
+    }
+
+    mymap.on('locationerror', onLocationError)
+    */
+
+    // Distane calculation demo
+    /*const myCustomIcon = L.icon({
+        iconUrl: './Data/icon_point.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -20]
+    })*/
+    /*
+    const myDivIcon = L.divIcon({
+        className: 'my-div-icon',
+        iconSize: 30
+    })
+
+    var counter = 0
+    var coordinates = []
+
+    mymap.on('click', function(e){
+        counter += 1
+        let latlng = e.latlng
+        coordinates.push(latlng)
+
+        let popup = L.popup({
+            autoClose: false,
+            closeOnClick: false
+        }).setContent(String(counter))
+    // Marker plotting
+        L.marker(latlng, {icon: myDivIcon}).addTo(mymap).bindPopup(popup).openPopup()
+
+        if (counter >= 2){
+            let distance = mymap.distance(coordinates[0], coordinates[1])
+            console.log(distance)
+            coordinates.shift()
+        }
+    })*/
+
+
+    // Draw Polyline
+    var drawPolyline = L.polyline([], {
+        color: 'red',
+        smoothFactor: 0.1
+    }).addTo(mymap)
+
+    mymap.on('click', function(e){
+        let latlng = e.latlng;
+        drawPolyline.addLatLng(latlng)
+    })
+
+    var masterPolyline = L.polyline([], {color: 'blue'}).addTo(mymap)
+    var masterLineCoordinates = []
+    mymap.on('dblclick', function(e){
+        let clickedAllCoordinates = drawPolyline.getLatLngs()
+        let clickedAllCoordinatesExceptTheLastOne = clickedAllCoordinates.slice(0, clickedAllCoordinates.length - 1)
+
+        masterLineCoordinates.push(clickedAllCoordinatesExceptTheLastOne)
+        masterPolyline.setLatLngs(masterLineCoordinates)
+
+        drawPolyline.setLatLngs([])
+
+        console.log(masterPolyline.toGeoJSON())
+    })
+
 }
