7c432cdd69609e460f5140cf349cb779d14695d3
sk8terdinz <d.refranto@gmail.com>
2023-08-23 07:01:52 +0700

    create fecth API function for load JSON data

--- main.js
+++ main.js
@@ -53,7 +53,8 @@ function init(){
     // Java marker
     const javaMarker = L.marker([-6.181483033062791, 106.82447481285564], {
         opacity: 1
-    }).addTo(mymap)
+    })
+    layerControls.addOverlay(javaMarker, 'Java')
 
     const javaMarkerPoppup = javaMarker.bindPopup('Jakarta city from popup')
     const javaMarkerTooltip = javaMarker.bindTooltip('Jakarta city from tooltip').openTooltip()
@@ -241,8 +242,121 @@ function init(){
     var svgElementBounds = [ [-8.455748978281614, 104.065442496],[-5.674229705192159, 106.901065401] ];
     L.svgOverlay(svgElement, svgElementBounds).addTo(mymap); */
 
-
+    // SVG example
     var svgURL = './Data/map-marker-23x31-active.svg';
     var svgBounds = [ [-8.455748978281614, 104.065442496],[-5.674229705192159, 106.901065401] ];
-    L.imageOverlay(svgURL, svgBounds).addTo(mymap)
+    var svgOverlay = L.imageOverlay(svgURL, svgBounds, {interactive: true})
+
+    // Circle layers
+    var circle = L.circle([50.5, 30.5], {radius:100000, color: 'red'})
+    var secondCircle = L.circleMarker([40.5, 40.5], {radius: 10})
+
+    /* // Layer Group
+    var layerGroup = L.layerGroup([circle, secondCircle]).addTo(mymap)
+    layerGroup.addLayer(svgOverlay)
+
+
+    layerGroup.eachLayer(function(layer){
+        let latLng;
+        // if the layer is SVG, get the center of the bounds
+        if (layer instanceof L.ImageOverlay){
+            latLng = layer.getBounds().getCenter()
+        }
+        // if the layer is circle, get lat and long
+        if (layer instanceof L.CircleMarker){
+            latLng = layer.getLatLng()
+        }
+         
+        let latitude = latLng.lat;
+        let longitude = latLng.lng;
+
+        let popup = L.popup({
+            autoClose: false,
+            closeOnClick: false
+        }).setContent(`Latitude is ${latitude}, longitude is ${longitude}`)
+
+        layer.bindPopup(popup).openPopup(latLng)
+
+
+        }) */
+    // Feature Group
+    var featureGroup = L.featureGroup([circle, secondCircle, svgOverlay])
+        .on('click', function(e){
+            console.log('Hey, you clicked on the featureGroup')
+        })
+    
+    layerControls.addOverlay(featureGroup, 'Feature Group')
+
+    featureGroup.bindPopup('Hi');
+
+    featureGroup.setStyle({
+        color: 'green'
+    })
+
+    // GeoJSON - point style
+    var pointStyle = {
+        radius:10,
+        color: 'green',
+        stroke: true,
+        weight:1,
+        fillColor: 'black',
+        fillOpacity:1
+    }
+
+    // Hoverstyle
+    var hoverStyle = {
+        radius:10,
+        color: 'blue',
+        stroke: true,
+        weight:1,
+        fillColor: 'yellow',
+        fillOpacity:1
+    }
+
+    // Function to add GeoJSON to the map
+    function addGeoJSONData(data, layername){
+        let geoJSONlayer = L.geoJSON(data, {
+            pointToLayer: function(feature, latlng){
+                return L.circle(latlng, pointStyle)
+            },
+
+            // filter option
+            filter: function(feature){
+                if (feature.properties.active === 'true'){
+                    return feature
+                }
+            }
+        })
+        
+        geoJSONlayer.bindPopup(function(layer){
+            return layer.feature.properties.name
+        })
+        geoJSONlayer.addTo(mymap)
+
+        geoJSONlayer.on('mouseover', function(e){
+            e.layer.setStyle(hoverStyle)
+        })
+        
+        layerControls.addOverlay(geoJSONlayer, layername)
+    }
+    // Fecth API
+    function fetchData(url, layername){
+        fetch(url, {
+            method: 'GET',
+            mode: 'same-origin'
+        })
+            .then(function(response){
+                if(response.status === 200){
+                    return response.json(response)
+                } else {
+                    throw new Error('fetch API could not fetch the data')
+                }
+            }).then(function(geojson){
+                addGeoJSONData(geojson, layername)
+            }).catch(function(error){
+                console.log(error)
+            })
+    }
+
+    fetchData('Data/map.geojson', 'POI')
 }
